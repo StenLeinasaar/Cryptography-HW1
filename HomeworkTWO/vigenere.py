59a7605f7b54966fdd0176e4b8b5971d4a63ddad
Sten Leinasaar <stenleinasaar@gmail.com>
2025-03-24 22:05:32 +0200

    adding homework 2

---
+++ HomeworkTWO/vigenere.py
@@ -0,0 +1,33 @@
+def vigenere_encrypt(text, key):
+    text = text.upper()
+    key = key.upper()
+    encrypted_text = ""
+    key_length = len(key)
+
+    for i, char in enumerate(text):
+        if char.isalpha():
+            shift = ord(key[i % key_length]) - ord("A")
+            encrypted_char = chr(((ord(char) - ord("A") + shift) % 26) + ord("A"))
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+
+    return encrypted_text
+
+
+# Given message and key
+message = "THEWANDCHOOSESTHEWIZARD"
+message2 = "THELANDCHOOSESTHELIZARD"
+key = "MAGIC"
+key2 = "MANIC"
+
+# Encrypt the message
+encrypted_message1 = vigenere_encrypt(message, key)
+encrypted_message2 = vigenere_encrypt(message2, key)
+print("Encrypted Message:", encrypted_message1)
+print("Encrypted Message:", encrypted_message2)
+
+encrypted_message1 = vigenere_encrypt(message, key2)
+encrypted_message2 = vigenere_encrypt(message2, key2)
+print("Encrypted with new key Message:", encrypted_message1)
+print("Encrypted with new key Message:", encrypted_message2)
